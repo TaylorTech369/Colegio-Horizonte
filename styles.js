b743619536af83fcdfec3bda707c3aeb0fb75f5e
TaylorTech369 <joaovictormendonca0411@gmail.com>
2025-11-03 17:14:26 -0300

    Add styles for login screen components

---
+++ styles.js
@@ -0,0 +1,64 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: "#FFF9F2",
+  },
+
+  botaozinho: {
+    backgroundColor: "#FFC88A",
+    borderRadius: 30,
+    padding: 10, 
+    width: 350,
+    marginTop: -10,
+  },
+
+  textodobotaozinho: {
+    textAlign: "center",
+    color: "#FFF5EB",
+    fontSize: 17,
+    fontStyle: "bold",
+  },
+
+  tituloLogin: {
+    color: "#FFC88A",
+    marginTop: 250,
+    fontSize: 55,
+  },
+
+  hrs: {
+    backgroundColor: "#FFC88A",
+    width: 100,
+  },
+
+  input1: {
+    // textAlign: "center",
+    color: "#FFC88A",
+    borderColor: "#FFC88A",
+    borderWidth: 1,
+    borderRadius: 30,
+    padding: 10, 
+    width: 350,
+    fontSize: 17,
+    marginBottom:10,
+  },
+
+  input2: {
+    color: "#FFC88A",
+    // textAlign: "center",
+    borderColor: "#FFC88A",
+    borderWidth: 1,
+    borderRadius: 30,
+    padding: 10, 
+    width: 350,
+    fontSize: 17,
+  },
+
+  inputs: {
+    padding: 10,
+    borderColor: "black",
+  },
+
+});
